739e717944fb18629f042a304e80d9c756eba7bc
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-14 19:24:27 -0300

    Adicionado funcao heapsort

---
+++ heapsort.py
@@ -0,0 +1,13 @@
+
+
+
+def heapsort(input_list):
+
+
+
+
+lista_ordenada_python = sorted(lista_para_ordenar)
+if lista_ordenada == lista_ordenada_python:
+    print("\nOrdenação correta!")
+else:
+    print("\nOrdenação INCORRETA!")
