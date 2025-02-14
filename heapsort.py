b8858f38a657b6aa83b779066f6e7877fe91ea8e
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-14 19:29:32 -0300

    Modificada funcao heapsort com instrucoes para ordenar uma lista

--- heapsort.py
+++ heapsort.py
@@ -2,9 +2,17 @@
 
 
 def heapsort(input_list):
+    tam = len(input_list)
+    
+    h = []
+    output_list = []
+    for i in range(0,tam):
+        heapq.heappush(h,input_list[i])
+    
+    for i in range(0,tam):
+        output = heappop(h)
 
-
-
+    return output_list
 
 lista_ordenada_python = sorted(lista_para_ordenar)
 if lista_ordenada == lista_ordenada_python:
