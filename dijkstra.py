d613ee4cdeb4c9025cd49b06483095ac4d953cda
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-17 17:41:12 -0300

    Adicionada funcao dijkstra

---
+++ dijkstra.py
@@ -0,0 +1,20 @@
+import heaqp
+import 
+
+def dijkstra(graph):
+
+
+	X = set()
+	H = []
+	 
+	for v in :
+		heapq.heappush(H,)
+		
+	while H :
+		X.add(heapq.heappop(H))
+		
+    for y in :
+        H.remove(y)
+        
+        heapq.heappush(y)
+	 
